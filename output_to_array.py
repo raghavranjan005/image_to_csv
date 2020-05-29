9a3c443b99da9f4d755ee3e7599400a94abc1335
rajutges <65604386+rajutges@users.noreply.github.com>
2020-05-30 01:46:02 +0530

    added output string to array conversion

---
+++ output_to_array.py
@@ -0,0 +1,40 @@
+
+s=input()
+f=s.split('\n')
+count=0
+d=list(f)
+
+for i in d:
+    for j in i:
+        if(j==" "):
+            count=count+1
+   
+    break
+
+p=s.split()
+
+Count1=0
+b=[]
+
+for i in range(0,len(p),count+1):
+    t=[]
+    for l in range(count+1):
+        c=p[i+l]
+        t.append(c)
+        
+    
+    b.append(t)
+    Count1=Count1+x
+print(b)
+Sum=0
+for i in range(len(b)):
+    for j in range(len(b[i])):
+        Sum=Sum+int(b[i][1])
+        break
+print(Sum)
+
+    
+    
+        
+            
+        
